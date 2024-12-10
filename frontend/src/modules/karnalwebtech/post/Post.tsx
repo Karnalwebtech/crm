a855af81a4dea5549b703e01d9340d35d8fe5211
pawan <pawan.irish@gmail.com>
2024-12-10 20:27:33 +0530

    aaaaa

--- frontend/src/modules/karnalwebtech/post/Post.tsx
+++ frontend/src/modules/karnalwebtech/post/Post.tsx
@@ -1,24 +1,26 @@
 "use client";
-import React, { useState } from "react";
-import Popover_component from "@/components/Popover_component/Popover_component";
+import React from "react";
 import PostList from "./post-list";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import CacheRemover from "@/components/common/CacheRemover";
+import { cache_keys } from "@/lib/service/custom_keys";
 
 const Post = () => {
   const router = useRouter();
 
+
   return (
     <div>
-      <div className="w-[150px] my-4">
+      <div className="w-[150px] my-4 flex gap-4">
         <Button
           className="bg-black text-white hover:text-black"
           onClick={() => router.push("/karnalwebtech/post/add-new")}
         >
           Add new post
         </Button>
+        <CacheRemover pattern={[cache_keys.posts]} />
       </div>
-
       <PostList />
     </div>
   );
