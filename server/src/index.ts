9a976980ab6fca6ca341b9581be02b07c25bb829
pawan <pawan.irish@gmail.com>
2025-02-10 12:25:04 +0530

    sdfsdf

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,34 +10,34 @@ connectRedis();
 const PORT = process.env.PORT || 8000;
 const numCPUs = os.cpus().length;
 
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`);
+// if (cluster.isPrimary) {
+//   console.log(`Primary ${process.pid} is running`);
 
-  // Fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+//   // Fork workers
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    cluster.fork(); // Replace the dead worker
-  });
-} else {
-  // Workers can share any TCP connection
-  // In this case, it is an HTTP server
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} died`);
+//     cluster.fork(); // Replace the dead worker
+//   });
+// } else {
+//   // Workers can share any TCP connection
+//   // In this case, it is an HTTP server
   app.listen(PORT, () => {
-    console.log(`Worker ${process.pid} is listening on port ${PORT}`);
+    console.log(`Worker is listening on port ${PORT}`);
   });
-}
+// }
 
-// Error handling for the primary process
-process.on("uncaughtException", (error) => {
-  console.error("Uncaught Exception:", error);
-  // Perform any cleanup operations here
-  process.exit(1);
-});
+// // Error handling for the primary process
+// process.on("uncaughtException", (error) => {
+//   console.error("Uncaught Exception:", error);
+//   // Perform any cleanup operations here
+//   process.exit(1);
+// });
 
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection at:", promise, "reason:", reason);
-  // Perform any cleanup operations here
-});
+// process.on("unhandledRejection", (reason, promise) => {
+//   console.error("Unhandled Rejection at:", promise, "reason:", reason);
+//   // Perform any cleanup operations here
+// });
